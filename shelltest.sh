a7f52df8f6bc722bcff31642f9ef84516b864432
hojintop <hojintop@gmail.com>
2021-04-22 15:53:51 +0900

    전체적인 명령어 정리 및 백업 & 반영 프로세스 구현테스트단계

--- shelltest.sh
+++ shelltest.sh
@@ -9,25 +9,40 @@ while read List
 		valueparam=`echo $List | rev | cut -d'/' -f1 | rev`
 	
 		# 파일명의 특수문자 여부 처리 '$' || '-' 만 확인 추가검증 필요시 조건 추가할것.	
-		if [[ "$List" =~ '$' ]] || [[ "$List" =~ '-' ]]; then
-			echo "특수문자확인"
-		else
-			echo "미확인"
-		fi
+		# 필요시 주석해제
+		#if [[ "$List" =~ '$' ]] || [[ "$List" =~ '-' ]]; then
+		#	echo "특수문자확인"
+		#else
+		#	echo "미확인"
+		#fi
+
 		# 현재 읽어올 txt 파일의 위치에 파일 존재유무를 판단
-		if [ -e '/$List' ]; then
-			
-			echo "file존재 백업진행"
-			
-			# 있다면 백업을 진행한다.
-			mv "$List" "$valueparam"'_20210422'
+		# $List 앞에 절대경로(/)를 붙여준이유는 어차피 절대경로를 가지로들어오며 특수문자가 있을수 있어 처리함.
+		if [ -e "/$List" ]; then
+			# 있다면 백업을 진행한다.	
+			echo "File존재 백업진행 : $List"
+			backupvalue="$List"'_'`date +%Y%m%d`
+			mv "$List" "$backupvalue"
+
+			if [ -e "/$backupvalue" ]; then
+				echo "백업완료"
+			else
+				echo "백업실패"
+			fi
 
 			cnt=$((cnt+1))
-                	echo "$valueparam"
 			# 해당파일 반영을 진행한다.
-                	cp "$valueparam" "$List" > ./test.log
+                	cp "$valueparam" "$List" 
+			
+			if [ -e "/$List" ]; then
+                                echo "반영완료: $valueparam"
+                        else
+                                echo "반영실패 : $valueparam"
+                        fi
+
+			> ./test.log
 		else
-			echo "$List file존재하지않음"
+			echo "기존File이존재하지않음 : $List"
 		fi 
 	fi
 	done < 위치.txt
